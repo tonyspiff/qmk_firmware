4751fb1fb6d8716da2150142d1493f9977262bbc
Tony Spiff <tonyspiff@floutstudio.com>
2017-09-17 23:44:57 -0700

    Added X Planck layout

---
+++ keyboards/planck/keymaps/x/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_X_H
+#define CONFIG_X_H
+
+#include "../../config.h"
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TIMEOUT 3000
+
+#undef LEADER_TIMEOUT
+#define LEADER_TIMEOUT 1500
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 250
+
+#endif
