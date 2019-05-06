fb74f9c5f66a7c5ae362dd56fbefb1af8538b5a2
Tony Spiff <tonyspiff@floutstudio.com>
2019-05-06 16:56:46 -0700

    Removed Arrows layer     + Fixed Ctrl combos     + Tweaked shortcuts

--- keyboards/ergodox/keymaps/x/ergodox_leds.h
+++ keyboards/ergodox/keymaps/x/ergodox_leds.h
@@ -15,6 +15,27 @@ enum LedColor
 	LedColorAll
 };
 
+// One Shot Modifiers
+#define isShiftOn (keyboard_report->mods & MOD_BIT(KC_LSFT) \
+			|| ((get_oneshot_mods() & MOD_BIT(KC_LSFT)) && !has_oneshot_mods_timed_out()) \
+			|| keyboard_report->mods & MOD_BIT(KC_RSFT) \
+			|| ((get_oneshot_mods() & MOD_BIT(KC_RSFT)) && !has_oneshot_mods_timed_out()))
+
+#define isCmdOn (keyboard_report->mods & MOD_BIT(KC_LCMD) \
+			|| ((get_oneshot_mods() & MOD_BIT(KC_LCMD)) && !has_oneshot_mods_timed_out()) \
+			|| keyboard_report->mods & MOD_BIT(KC_RCMD) \
+			|| ((get_oneshot_mods() & MOD_BIT(KC_RCMD)) && !has_oneshot_mods_timed_out()))
+
+#define isCtrlOn (keyboard_report->mods & MOD_BIT(KC_LCTL) \
+			|| ((get_oneshot_mods() & MOD_BIT(KC_LCTL)) && !has_oneshot_mods_timed_out()) \
+			|| keyboard_report->mods & MOD_BIT(KC_RCTL) \
+			|| ((get_oneshot_mods() & MOD_BIT(KC_RCTL)) && !has_oneshot_mods_timed_out()))
+
+#define isOptionOn (keyboard_report->mods & MOD_BIT(KC_LALT) \
+			|| ((get_oneshot_mods() & MOD_BIT(KC_LALT)) && !has_oneshot_mods_timed_out()) \
+			|| keyboard_report->mods & MOD_BIT(KC_RALT) \
+			|| ((get_oneshot_mods() & MOD_BIT(KC_RALT)) && !has_oneshot_mods_timed_out()))
+
 void toggleLed(bool isOn, uint8_t color)
 {
 	switch(color)
