6508f28b4c9a54e8459fee1c546c9640d252d318
Tony Spiff <tonyspiff@floutstudio.com>
2019-07-03 22:13:18 -0700

    Disabled some features to shrink firmware size

--- keyboards/ergodox/keymaps/x/config.h
+++ keyboards/ergodox/keymaps/x/config.h
@@ -1,14 +1,23 @@
 #ifndef CONFIG_X_H
 #define CONFIG_X_H
 
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif
+
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif
+
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
 
 #define ONESHOT_TAP_TOGGLE 2
-#undef ONESHOT_TIMEOUT
 #define ONESHOT_TIMEOUT 750
 
-#undef LEADER_TIMEOUT
 #define LEADER_TIMEOUT 750
 #define LEADER_PER_KEY_TIMING
 
